798abb8fcf86dc35e7fdb8db017832d44d86c255
victor <pshkab@gmail.com>
2017-06-15 17:00:10 +0800

    vue-router

---
+++ vue-webpack/src/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-bind:value="msg" v-on:input="changeMessage">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: ['msg'],
+      data() {
+          return {
+              message: ''
+          }
+      },
+      methods: {
+          changeMessage(){
+              this.message = event.target.value;
+              this.$emit('messageChanged',this.message);
+          }
+      }
+  }
+</script>
